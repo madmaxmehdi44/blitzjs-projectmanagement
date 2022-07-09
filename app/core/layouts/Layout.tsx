fcd26896ff14f575eabbf0c524a376c1c4287484
madmaxmehdi44 <madmaxmehdi44@gmail.com>
2022-07-10 02:17:17 +0430

    layout

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from "react"
-import { Head, BlitzLayout } from "blitz"
+import { Head, RedirectAuthenticatedTo, RedirectAuthenticatedToFn, RouteUrlObject } from "blitz"
 // import { Header } from "../components/Header"
 import NavBar from "../components/NavBar"
 import ModalIndex from "../components/ModalCompnent"
 import Footer from "../components/Footer"
 
+export type BlitzLayout<P = {}> = React.ComponentType<P> & {
+  authenticate?: boolean | { redirectTo?: string | RouteUrlObject }
+  redirectAuthenticatedTo?: RedirectAuthenticatedTo | RedirectAuthenticatedToFn
+}
 // type LayoutProps = {
 //   title?: string
 //   children: ReactNode
