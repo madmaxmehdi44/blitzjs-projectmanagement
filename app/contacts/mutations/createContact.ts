0c37bec0f425272a550f17b9229a20b310f31bc5
madmaxmehdi44 <madmaxmehdi44@gmail.com>
2022-07-15 01:12:02 +0430

    ContactsAdded

---
+++ app/contacts/mutations/createContact.ts
@@ -0,0 +1,18 @@
+import { resolver } from "blitz"
+import db from "db"
+import { z } from "zod"
+
+const CreateContact = z.object({
+  name: z.string(),
+})
+
+export default resolver.pipe(
+  resolver.zod(CreateContact),
+  // resolver.authorize(),
+  async (input) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const contact = await db.contact.create({ data: input })
+
+    return contact
+  }
+)
---
+++ app/contacts/queries/getContacts.ts
@@ -0,0 +1,30 @@
+import { paginate, resolver } from "blitz"
+import db, { Prisma } from "db"
+
+interface GetContactsInput
+  extends Pick<Prisma.ContactFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
+
+export default resolver.pipe(
+  resolver.authorize(),
+  async ({ where, orderBy, skip = 0, take = 100 }: GetContactsInput) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const {
+      items: contacts,
+      hasMore,
+      nextPage,
+      count,
+    } = await paginate({
+      skip,
+      take,
+      count: () => db.contact.count({ where }),
+      query: (paginateArgs) => db.contact.findMany({ ...paginateArgs, where, orderBy }),
+    })
+
+    return {
+      contacts,
+      nextPage,
+      hasMore,
+      count,
+    }
+  }
+)
